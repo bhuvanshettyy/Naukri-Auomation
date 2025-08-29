199f30cc32340601032cc57c8d1b1d84709f04bd
bhuvan <bhuvanshettyy@gmail.com>
2025-08-29 20:11:41 +0530

    patch on workflow(Run Script)

--- main.py
+++ main.py
@@ -17,19 +17,28 @@ chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-# find chromium path dynamically
+
+# Detect Chromium binary
 chromium_path = shutil.which("chromium-browser") or shutil.which("chromium")
 if chromium_path is None:
     raise FileNotFoundError("Chromium not found in runner")
 chrome_options.binary_location = chromium_path
-driver = webdriver.Chrome(options=chrome_options)
+
+# Detect chromedriver
+chromedriver_path = shutil.which("chromedriver")
+if chromedriver_path is None:
+    raise FileNotFoundError("Chromedriver not found in runner")
+
+# Use both explicitly
+driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+
 driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid")
-driver.maximize_window()
 driver.implicitly_wait(5)
 
 driver.find_element(By.ID, "usernameField").send_keys(NAUKRI_EMAIL)
 driver.find_element(By.ID, "passwordField").send_keys(NAUKRI_PASSWORD)
 driver.find_element(By.XPATH, "//*[@id='loginForm']/div[2]/div[3]/div/button[1]").click()
+
 WebDriverWait(driver, 10).until(
     EC.element_to_be_clickable((By.XPATH, "//*[@id='portal']/div/div[2]/div/img"))
 ).click()
@@ -44,7 +53,6 @@ RESUME_PATH = os.environ.get("RESUME_PATH", "Bhuvan_Resume.pdf")
 file_path = os.path.join(os.getcwd(), RESUME_PATH)
 if not os.path.exists(file_path):
     raise FileNotFoundError(f"File not found: {file_path}")
-
 
 uploadInput = WebDriverWait(driver, 20).until(
     EC.presence_of_element_located((By.CSS_SELECTOR, "input[type='file'].upload-input"))
