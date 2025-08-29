197fe460d71ac684dabdfb3b2e5b42191124f928
bhuvan <bhuvanshettyy@gmail.com>
2025-08-29 20:05:16 +0530

    patch on workflow(Run Script)

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
 git add .
-git commit -m "patch on workflow"
+git commit -m "patch on workflow(Run Script)"
 git push origin master
--- main.py
+++ main.py
@@ -16,6 +16,7 @@ chrome_options.add_argument("--headless=new")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/chromium-browser"
 driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid")
 driver.maximize_window()
